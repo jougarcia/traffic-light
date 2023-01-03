559729f932417ef4dfa9251a3e534ced557e52df
jougarcia <jougarcia95@gmail.com>
2023-01-03 11:58:08 +0000

    joseph

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import TrafficLight from "./semaforo.jsx";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Semaforo from "./semaforo.jsx";
 
-//create your first component
+
 const Home = () => {
 	return (
 		<Semaforo />
